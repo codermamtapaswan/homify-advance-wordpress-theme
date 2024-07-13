d02a7ef6b7561824464312f2214882d54fc1d380
Mamta Paswan <mamtapaswan2021@gmail.com>
2024-07-13 18:54:31 +0530

    Set traits-sigleton

--- function.php
+++ function.php
@@ -19,25 +19,25 @@ if (!defined('ABSPATH')) {
 /*  Theme Contants
 ---------------------------*/
 
-if (!defined('Homify_DIR_PATH')) {
-  define('Homify_DIR_PATH', untrailingslashit(get_template_directory()));
+if (!defined('HOMIFY_DIR_PATH')) {
+  define('HOMIFY_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
-if (!defined('Homify_DIR_URI')) {
-  define('Homify_DIR_URI', untrailingslashit(get_template_directory_uri()));
+if (!defined('HOMIFY_DIR_URI')) {
+  define('HOMIFY_DIR_URI', untrailingslashit(get_template_directory_uri()));
 }
 
-if (!defined('Homify_HOME_URI')) {
-  define('Homify_HOME_URI', esc_url(home_url('/')));
+if (!defined('HOMIFY_HOME_URI')) {
+  define('HOMIFY_HOME_URI', esc_url(home_url('/')));
 }
 
 
-require_once Homify_DIR_PATH . '/inc/helpers/autoloader.php';
-require_once Homify_DIR_PATH . '/inc/helpers/template-tags.php';
+require_once HOMIFY_DIR_PATH . '/inc/helpers/autoloader.php';
+require_once HOMIFY_DIR_PATH . '/inc/helpers/template-tags.php';
 
 
-function Homify_get_theme_instance()
+function homify_get_theme_instance()
 {
-  \Homify_THEME\Inc\Homify_THEME::get_instance();
+  \HOMIFY_THEME\Inc\HOMIFY_THEME::get_instance();
 }
-Homify_get_theme_instance();
+homify_get_theme_instance();
